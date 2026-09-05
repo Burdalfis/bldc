7ba9138e63d9991123391bbdebc27f1a5d19d491
Burdalfis <xelastlaurent@gmail.com>
2026-09-05 16:59:18 -0300

    Add MINI4 B-H tracer module header

---
+++ hwconf/other/hw_mini4_bh.h
@@ -0,0 +1,9 @@
+#ifndef HW_MINI4_BH_H_
+#define HW_MINI4_BH_H_
+
+#include "hw_mini4_bh_core.inc"
+#include "hw_mini4_bh_measure.inc"
+#include "hw_mini4_bh_capture.inc"
+#include "hw_mini4_bh_terminal.inc"
+
+#endif /* HW_MINI4_BH_H_ */
