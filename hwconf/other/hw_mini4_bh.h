a7c0415269291cd67683b8942fffba973576252a
Burdalfis <xelastlaurent@gmail.com>
2026-09-05 18:21:13 -0300

    Wire programmable gains and inductance matrix extension

--- hwconf/other/hw_mini4_bh.h
+++ hwconf/other/hw_mini4_bh.h
@@ -3,6 +3,7 @@
 
 #include "utils_sys.h"
 #include "hw_mini4_bh_core.inc"
+#include "hw_mini4_bh_policy.inc"
 
 /*
  * timer_sleep() in VESC is a high-resolution busy-wait intended for very
@@ -27,10 +28,31 @@ static inline void bh_rtos_sleep(float seconds) {
 }
 
 #define timer_sleep(seconds) bh_rtos_sleep(seconds)
+
+/* Keep the original implementations compiled as fallbacks, then provide the
+ * v2 policy-driven R/L worker and the diagnostic current/frequency sweep.
+ */
+#define bh_measure_rl_locked bh_measure_rl_locked_legacy
 #include "hw_mini4_bh_measure.inc"
+#undef bh_measure_rl_locked
+#include "hw_mini4_bh_measure_v2.inc"
+
+#define bh_run_one bh_run_one_legacy
+#define bh_print_metrics bh_print_metrics_legacy
 #include "hw_mini4_bh_capture.inc"
+#undef bh_run_one
+#undef bh_print_metrics
+#include "hw_mini4_bh_capture_v2.inc"
+
 #undef timer_sleep
 
+/* Extend the command table without modifying the legacy terminal module. */
+#define bh_init_commands bh_init_commands_legacy
 #include "hw_mini4_bh_terminal.inc"
+#undef bh_init_commands
+#include "hw_mini4_bh_terminal_v2.inc"
+
+#undef bh_set_measurement_gains
+#undef bh_set_run_gains
 
 #endif /* HW_MINI4_BH_H_ */
