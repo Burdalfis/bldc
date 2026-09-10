dc019c43ac134dde7ef45c9ce9f473c59f27b73e
Burdalfis <xelastlaurent@gmail.com>
2026-09-09 21:00:58 -0300

    Drop legacy BLDC MCPWM from MINI4 tracer build

--- motor/motor.mk
+++ motor/motor.mk
@@ -1,9 +1,14 @@
 CSRC += \
 	motor/foc_math.c \
 	motor/mc_interface.c \
-	motor/mcpwm.c \
 	motor/mcpwm_foc.c \
 	motor/virtual_motor.c
+
+ifeq ($(PROJECT),mini4)
+CSRC += motor/mcpwm_stub.c
+else
+CSRC += motor/mcpwm.c
+endif
 	
 INCDIR += motor
 
